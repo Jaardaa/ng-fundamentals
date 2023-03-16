2cc7aab76238ac0cdc527220ff0b2450d7386643
Jaardaa <78655691+Jaardaa@users.noreply.github.com>
2023-03-16 17:32:37 +0100

    Images added into Assets

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'events-app',
-  template: '<h2>Hello World'
+  template: `
+  <h2>Hello World
+  <img src="../assets/images/basic-shield.png">
+  `
 
 })
 export class EventsAppComponent {

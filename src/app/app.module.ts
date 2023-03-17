448e69f18d6e104e7dc1bef20b2f1e5b0f47a891
Jaardaa <78655691+Jaardaa@users.noreply.github.com>
2023-03-17 18:11:52 +0100

    Module 3 finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { EventsAppComponent } from './events-app.component';
+import { EventListComponent } from './events/event-list/event-list.component';
+import { EventThumbnailComponent } from './events/event-thumbnail/event-thumbnail.component';
+import { NavbarComponent } from './nav/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    EventsAppComponent
+    EventsAppComponent,
+    EventListComponent,
+    EventThumbnailComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -3,11 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'events-app',
   template: `
-  <h2>Hello World
-  <img src="../assets/images/basic-shield.png">
+  <app-navbar></app-navbar>
+  <app-event-list></app-event-list>
   `
 
 })
 export class EventsAppComponent {
   title = 'ng-fundamentals';
+
+
 }
